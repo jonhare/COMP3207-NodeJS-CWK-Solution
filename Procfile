961480edb6c5724bd9c12afb15ad77bf7f32198e
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2014-08-12 13:59:11 +0100

    exploring ruppells and jade

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: node web.js
+web: ./lib/sockets-connect/rs-conn ./web_api.js
+socket: ./lib/sockets-connect/rs-conn ./tcp_listener.js
--- web.js
+++ web.js
@@ -1,14 +1,26 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var net = require('net');
+
+//configure ports
+var tcpPort = Number(process.env.RUPPELLS_SOCKETS_LOCAL_PORT || 1337);
+var httpPort = Number(process.env.PORT || 5000);
+
+var tcpServer = process.env.RUPPELLS_SOCKETS_FRONTEND_URI || "localhost:" + tcpPort;
+
+//Setup the HTTP application
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index.jade', {tcpURI: tcpURI})
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
+app.listen(httpPort, function() {
   console.log("Listening on " + port);
 });
+
+//Setup the tcp server
+net.createServer(socketHandler).listen(ruppells_sockets_local_port);
+
